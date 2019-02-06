7a24e8682987a098e6052721bfdc43d905e8ea56
Cox, Addison W <Addison.W.Cox-1@ou.edu>
2019-02-06 11:03:36 -0600

    Commit 1

---
+++ WPF1/WPF1/Application.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace WPF1
+{
+    class Application
+    {
+        public string Name {get; set;}
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+
+    public Application()
+    {
+        Name = " ";
+
+    }
+
+
+}
+
+
+
